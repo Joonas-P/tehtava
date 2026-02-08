feff5f2092d7d12857af9d0a65721e1ebc0b48b9
joonas <joonas.paavola2@metropolia.fi>
2026-02-08 10:58:39 +0200

    viikko2.3.py

---
+++ viikko2/viikko2.3.py
@@ -0,0 +1,10 @@
+import math
+kanta = float(input("Anna suorakulmion kanta: "))
+korkeus = float(input("Anna suorakulmion korkeus: "))
+piiri = 2 * (kanta + korkeus)
+pinta_ala = kanta * korkeus
+print("suorakulmion piiri on:", piiri)
+print("suorakulmion pinta_ala on:", pinta_ala)
+
+
+
