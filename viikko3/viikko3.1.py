3ddca449c9e65f45b7850f8a5a66677f07a9a39a
joonas <joonas.paavola2@metropolia.fi>
2026-02-12 10:30:17 +0200

    viikko3.py

---
+++ viikko3/viikko3.1.py
@@ -0,0 +1,4 @@
+pituus = float(input("kuhan pituus:"))
+if pituus <= 37:
+    print("Kuha on alamittainen, VAPAUTA SE!!")
+print("Kuhan alamitasta puuttuu", 37 - pituus, "cm")
---
+++ viikko3/viikko3.4.py
@@ -0,0 +1,10 @@
+import math
+vuosi = float(input("Anna vuosi: "))
+if (vuosi % 4 == 0 and vuosi % 100 != 0) or (vuosi % 400 == 0 ): print("vuosi on karkausvuosi")
+
+else:
+    print("vuosi ei ole karkausvuosi")
+
+
+
+
